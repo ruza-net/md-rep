5db3c15b85c962410b7d5895ea0c8b3798c93328
Jan Růžička <jan.ruzicka01@gmail.com>
2025-11-28 14:38:14 +0100

    multiple blanks on a line

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use std::io;
 use std::io::{Read, Seek, Write};
 use std::path::Path;
 
-fn is_yes(s: &str) -> bool {
+fn is_yes(s: &str, empty_is_yes: bool) -> bool {
+    if empty_is_yes && s.trim().is_empty() {
+        return true;
+    }
     let s = s.to_lowercase();
     match s.trim() {
         "yes" | "y" => true,
@@ -20,7 +23,7 @@ fn open_or_create(name: &str) -> Option<File> {
         Ok(f) => Some(f),
 
         Err(_) => {
-            print!("`{}` not found. Do you want to create it? ", name);
+            print!("`{}` not found. Do you want to create it? [y/n] ", name);
             io::stdout().flush().unwrap();
 
             let stdin = io::stdin();
@@ -28,12 +31,12 @@ fn open_or_create(name: &str) -> Option<File> {
             stdin.read_line(&mut buf).unwrap();
 
             let answer = buf.to_lowercase();
-            if is_yes(&answer) {
+            if is_yes(&answer, false) {
                 println!("OK");
                 let path = Path::new(name);
                 std::fs::create_dir_all(path.parent().unwrap()).unwrap();
                 File::create(path).unwrap();
-                File::open(path).ok()
+                OpenOptions::new().read(true).append(true).open(name).ok()
             } else {
                 None
             }
@@ -141,37 +144,55 @@ fn rep(name: &str) -> i64 {
     let blank = regex::Regex::new(r"\?\[(([^\[\]]|\\\[\\\])*)\]").unwrap();
     let lines: Vec<_> = text.lines().collect();
     for (line_no, line) in lines.iter().enumerate() {
-        // TODO: handle multiple blanks on a line
+        let matches = blank.captures_iter(line);
+        let matches: Vec<_> = matches.collect();
+
+        if matches.len() == 0 {
+            continue;
+        }
+
+        // Print some lines before the blank
         //
-        if let Some(m) = blank.captures(line) {
-            // Print some lines before the blank
-            //
-            println!("@-------");
+        println!("@-------");
+        let ctx_start = line_no.saturating_sub(ctx_len);
+        let ctx_end = (line_no + ctx_len).min(lines.len());
+        println!(
+            "{}",
+            lines[ctx_start..line_no]
+                .iter()
+                .map(|l| String::from("  ") + l)
+                .fold(String::new(), |a, b| a + &b)
+        );
 
-            let ctx_start = line_no.saturating_sub(ctx_len);
-            let ctx_end = (line_no + ctx_len).min(lines.len());
-            println!(
-                "{}",
-                lines[ctx_start..line_no]
-                    .iter()
-                    .map(|l| String::from("  ") + l)
-                    .fold(String::new(), |a, b| a + &b)
-            );
-            let prefix = &line[..m.get(0).unwrap().start()];
-            let suffix = &line[m.get(0).unwrap().end()..];
-            println!("  {}{}{}", prefix, "____", suffix);
+        // Print line with blanks
+        //
+        let mut blanks = vec![];
+        let mut line_segs = vec![];
+        let mut last_end = 0;
+        for m in matches {
+            let start = m.get_match().start();
+            let end = m.get_match().end();
+            let ans = m.get(1).unwrap().as_str();
+            blanks.push(ans);
+            line_segs.push(&line[last_end..start]);
+            line_segs.push("____");
+            last_end = end;
+        }
+        line_segs.push(&line[last_end..]);
+        println!("{}", line_segs.join(""));
 
-            // Print some lines after the blank
-            //
-            println!(
-                "{}",
-                lines[line_no + 1..ctx_end]
-                    .iter()
-                    .map(|l| String::from("  ") + l)
-                    .fold(String::new(), |a, b| a + &b)
-            );
-            println!("@-------");
+        // Print some lines after the blank
+        //
+        println!(
+            "{}",
+            lines[line_no + 1..ctx_end]
+                .iter()
+                .map(|l| String::from("  ") + l)
+                .fold(String::new(), |a, b| a + &b)
+        );
+        println!("@-------");
 
+        for blank in blanks {
             // Query an answer
             //
             print!("> ");
@@ -181,16 +202,15 @@ fn rep(name: &str) -> i64 {
 
             // Diff to expected
             //
-            println!("+ {}{}{}", prefix, m.get(1).unwrap().as_str(), suffix);
-            println!("- {}{}{}", prefix, answer.trim(), suffix);
+            println!("+ {}", blank);
 
             // User feedback
             //
-            print!("@ Did you get it correct? ");
+            print!("@ Did you get it correct? [Y/n] ");
             io::stdout().flush().unwrap();
             answer.clear();
             stdin.read_line(&mut answer).unwrap();
-            if is_yes(&answer) {
+            if is_yes(&answer, true) {
                 score += 1;
             } else {
                 score -= 1;
